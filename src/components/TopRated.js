c57d0ee8fdab35c23c57f13bd3763ffcb1be1530
Abhay Sharma <118429094+Abhay-04@users.noreply.github.com>
2024-11-03 15:51:09 +0530

    Initial commit

---
+++ src/components/TopRated.js
@@ -0,0 +1,24 @@
+import React from "react";
+import RestaurantCard from "./RestaurantCard";
+import { Link } from "react-router-dom";
+
+const TopRated = ({ data }) => {
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Top Rated Restaurants</h1>
+      {data.map((item) => (
+            <div key={item?.info?.id}>
+              <Link to={"/restaurant/" + item?.info?.id}>
+                <div className="mx-4">
+                  <RestaurantCard resData={item} />
+                </div>
+              </Link>
+            </div>
+          ))}
+    </div>
+  );
+};
+
+export default TopRated;
